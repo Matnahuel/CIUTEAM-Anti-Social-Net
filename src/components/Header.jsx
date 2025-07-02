8c78c579e7b6b60f21355475b2c89936e67d1ee7
IRojas99 <ivanemiliano.rojas@estudiantes.unahur.edu.ar>
2025-07-02 20:06:58 -0300

    Se arregló el header para que aparezcan las opciones solo si se está logueado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,33 @@
-
+import { useAuth } from "../contexts/authContext";
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'; 
 
 function Header() { 
+  const { usuario } = useAuth();
+
   return (
-    <header className="main-header"> {}
+    <>
+     <header className="main-header">
       <div className="header-container">
-        <Link to="/home" className="app-logo"> {}
-          UnaHur Anti-Social Net
+        <Link to="/home" className="app-logo">
+          UnaHur Anti‑Social Net
         </Link>
-        <nav className="main-nav"> {}
-          <Link to="/home">Inicio</Link>
-          <Link to="/perfil">Perfil</Link> {}
-          <Link to="/crear-post">Crear Post</Link> {}
+
+        <nav className="main-nav">
+          {usuario ? (                       
+            <>
+              <Link to="/home">Inicio</Link>
+              <Link to={`/perfil`}>Perfil</Link> 
+              <Link to="/crear-post">Crear Post</Link>
+            </>
+          ) : (                        
+            <> </>
+          )}
         </nav>
       </div>
     </header>
+    </>
   );
 }
 
